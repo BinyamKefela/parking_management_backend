874b84fa71ec6fc376b991dd349e32004922e272
BinyamKefela <binyamkefela196@gmail.com>
2025-06-16 00:08:39 -0700

    monday start

--- parking_management/vpms/api/subscription.py
+++ parking_management/vpms/api/subscription.py
@@ -25,7 +25,8 @@ class SubscriptionListView(generics.ListAPIView):
     #filterset_fields = '__all__'
     #search_fields = [field.name for field in Subscription._meta.fields]
     filterset_fields = {
-    'owner__company_owner__id': ['exact',],
+    'owner__company_owner__id': ['exact'],
+    'status': ['exact']
     }
     search_fields = ["owner__company_owner__email","owner__company_name"]
     ordering_fields = [field.name for field in Subscription._meta.fields]
